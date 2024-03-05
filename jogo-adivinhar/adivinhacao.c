2914bef3b86eddfeb2f11a30049da15342bc9d1b
henriqueAlgauer <algauer.henrique@gmail.com>
2024-03-05 00:44:27 -0300

    bug fix do sistema de pontos para float

--- jogo-adivinhar/adivinhacao.c
+++ jogo-adivinhar/adivinhacao.c
@@ -23,8 +23,8 @@ int main(){
     int num_secreto = 20;
     int chute;
     int num_tentativa = 0;
-    int pontos = 1000;
-    int pontos_perdidos = 0;
+    float pontos = 1000;
+    float pontos_perdidos = 0;
 
     while(chute != num_secreto){
         printf("\nTentativa %d", num_tentativa+1);
@@ -40,19 +40,19 @@ int main(){
             printf("Parabéns! Você acertou !");
         }else if(chute > num_secreto){
             printf("Você errou !\nSeu chute foi maior que o numero secreto");
-            pontos_perdidos =  (chute - num_secreto) / 2;
+            pontos_perdidos =  (chute - num_secreto) / 2.0;
             pontos -= pontos_perdidos;
         }else{
             printf("Você errou !\nSeu chute foi menor que o numero secreto");
-            pontos_perdidos =  (num_secreto - chute) / 2;
+            pontos_perdidos =  (num_secreto - chute) / 2.0;
             pontos -= pontos_perdidos;
         }
         
         num_tentativa++;
     }
 
-    printf("\nVocê acertou na tentativa numero: %d", num_tentativa);
-    printf("\nSua pontuação foi: %d", pontos);
+    printf("\nVocê acertou em %d tentativas !", num_tentativa);
+    printf("\nSua pontuação foi: %.1f", pontos);
     printf("\nFim de jogo");
     return 0;
 }
