d7c00c9b189ff1dde697baee3d4ea3f3422f7b55
henriqueAlgauer <algauer.henrique@gmail.com>
2024-03-07 09:11:27 -0300

    .

--- jogo-pac-man/fogefoge.c
+++ jogo-pac-man/fogefoge.c
@@ -1,14 +1,26 @@
-
 #include <stdio.h>
 #include <stdlib.h>
+#include "fogefoge.h"
 
 char** mapa;
 
 int linhas;
 int colunas;
 
-int main(){
+void liberamapa(){
+    for(int i =0; i<linhas; i++){
+        free(mapa[i]);
+    }
+}
 
+void alocamapa(){
+    mapa = malloc(sizeof(char*)*linhas);
+    for(int i=0; i<linhas; i++){
+        mapa[i] = malloc(sizeof(char)* (colunas+1));
+    }
+}
+
+void lermapa(){
     FILE* f;
     f = fopen("D:/VisualStudioCODE/C/jogo-pac-man/mapa.txt","r");
     if(f==0){
@@ -17,25 +29,22 @@ int main(){
     }
 
     fscanf(f, "%d %d", &linhas, &colunas);
-    printf("linhas %d colunas %d\n", linhas, colunas);
-
-    //////////
-    mapa = malloc(sizeof(char*)*linhas);
-    for(int i=0; i<linhas; i++){
-        mapa[i] = malloc(sizeof(char)* (colunas+1));
-    }
-    //////////    
+    alocamapa();
 
     for(int i =0;i<5;i++){
         fscanf(f,"%s", mapa[i]);
     }
 
+    fclose(f);
+}
+
+int main(){
+
+    lermapa();
+    
     for(int i =0;i<5;i++){
         printf("%s\n", mapa[i]);
     }
 
-    fclose(f);
-    for(int i =0; i<linhas; i++){
-        free(mapa[i]);
-    }
+    liberamapa();
 }
